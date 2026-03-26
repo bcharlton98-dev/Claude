172c2ceace676a3424f055fd7a560f0b82976337
bcharlton98-dev <bcharlton98@gmail.com>
2026-03-26 12:32:18 -0400

    chore: change password to strawberry

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import JobSearch from './pages/JobSearch'
 //   crypto.subtle.digest('SHA-256', new TextEncoder().encode('yourpassword'))
 //     .then(b => console.log(Array.from(new Uint8Array(b)).map(x => x.toString(16).padStart(2,'0')).join('')))
 // Then replace the hash below.
-const PASSWORD_HASH = 'a3ebd9ee973efc656a1ccd64b6b34e314bd1f8a296178de7be04702e0321802a'
+const PASSWORD_HASH = '5e737f891db1175442a39fde73e51d781a545506d71c95477a6deb5988bd7f9a'
 
 export default function App() {
   return (
